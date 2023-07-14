cfa5d42ab1a1e6095ccfbf4adaa83147336fef26
Tobias Pflug <tobias.pflug@gmail.com>
2023-07-14 16:01:32 +0200

    Implement variable declaration

--- src/Silly/Eval.hs
+++ src/Silly/Eval.hs
@@ -2,11 +2,11 @@
 
 module Silly.Eval where
 
-import Control.Monad.Except (ExceptT, MonadError (..),runExceptT)
+import Control.Monad.Base (MonadBase)
+import Control.Monad.Except (ExceptT, MonadError (..), runExceptT)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.State.Strict (MonadState, StateT, evalStateT)
-import Control.Monad.Base (MonadBase)
-import qualified Control.Monad.State.Strict as State
+import Control.Monad.State.Strict qualified as State
 import Data.IORef.Lifted
 import Data.Map.Strict qualified as Map
 import Data.Text qualified as T
@@ -15,25 +15,31 @@ import Silly.Expr
 newtype SillyException = Exception T.Text
   deriving (Eq, Show)
 
+-- | Runtime values that have been evaluated from Expressions
 data SillyVal
-  = IntValue Int
-  | StrValue T.Text
-  | BoolValue Bool
+  = -- | a number value
+    IntValue Int
+  | -- | a string value
+    StrValue T.Text
+  | -- | a boolean value
+    BoolValue Bool
   deriving (Show, Eq)
 
--- The result of an evaluation is either an error text or
--- a SillyVal
-type EvalResult = Either T.Text SillyVal
-
+-- | Evaluation environment.
+-- The evaluation environment will eventually store variables or
+-- functions during runtime.
 type Env = Map.Map Identifier (IORef SillyVal)
 
+-- | Creates an initial environment.
+-- The initial environment provides builtin functions and variable declarations
 initialState :: IO InterpreterState
 initialState = do
   meaning <- newIORef (IntValue 42)
-  return $ InterpreterState $
-    Map.fromList
-      [ ("meaning", meaning)
-      ]
+  return $
+    InterpreterState $
+      Map.fromList
+        [ ("meaning", meaning)
+        ]
 
 newtype InterpreterState = InterpreterState {sillyEnv :: Env}
 
@@ -50,14 +56,17 @@ newtype Interpreter a = Interpreter
       MonadError SillyException
     )
 
+-- | Runs an expression evaluation through the Interpreter monad stack
 runEvalExpr :: SillyExpr -> IO (Either SillyException SillyVal)
 runEvalExpr exp = do
   s <- initialState
   flip evalStateT s . runExceptT . runInterpreter $ evalExpr exp
 
+-- | Throws an Exception in the Interpreter monad stack with the given description
 runtimeError :: T.Text -> Interpreter a
 runtimeError = throwError . Exception
 
+-- | Evaluates an expression
 evalExpr :: SillyExpr -> Interpreter SillyVal
 evalExpr = \case
   LitInt x -> return $ IntValue x
@@ -66,15 +75,37 @@ evalExpr = \case
   BinaryOp op l r -> evalBinOp op l r
   Variable varId -> lookupVar varId
 
+-- | Retrieves a variable specified by identifier.
+-- An runtime exception will be thrown if the identifier is not present in the runtime environment
 lookupVar :: Identifier -> Interpreter SillyVal
 lookupVar varId = State.gets sillyEnv >>= getRef varId >>= readIORef
 
+-- | Obtain a reference from the environment
 getRef :: Identifier -> Env -> Interpreter (IORef SillyVal)
 getRef idVar env =
   case Map.lookup idVar env of
-       Just ref -> return ref
-       Nothing -> runtimeError "undefined variable"
+    Just ref -> return ref
+    Nothing -> runtimeError "undefined variable"
 
+-- | Execute a statement in the interpreter
+runStatement :: Stmt -> Interpreter ()
+runStatement = \case
+  ExprStmt expr -> State.void $ evalExpr expr
+  VarStmt varId expr -> evalExpr expr >>= defineVar varId
+
+-- | Update the Interpreter State environment
+updateEnv :: Env -> Interpreter ()
+updateEnv env = State.modify' $ \s -> s {sillyEnv = env}
+
+-- | Execute a variable declaration by name and value
+defineVar :: Identifier -> SillyVal -> Interpreter ()
+defineVar varId val = do
+  env <- State.gets sillyEnv
+  if Map.member varId env
+    then runtimeError $ "variable '" <> varId <> "' has already been defined"
+    else newIORef val >>= \ref -> updateEnv $ Map.insert varId ref env
+
+-- | Evaluate a binary operation expression
 evalBinOp :: BinOp -> SillyExpr -> SillyExpr -> Interpreter SillyVal
 evalBinOp op l r = do
   leftVal <- evalExpr l
--- src/Silly/Expr.hs
+++ src/Silly/Expr.hs
@@ -1,21 +1,46 @@
-module Silly.Expr   where
+module Silly.Expr where
 
-import qualified Data.Text as T
+import Data.Text qualified as T
 
 type Identifier = T.Text
 
-data SillyExpr = LitInt Int
-               | LitString T.Text
-               | LitBool Bool
-               | Variable Identifier
-               | BinaryOp BinOp SillyExpr SillyExpr
-               deriving (Show, Eq)
+-- | Expressions supported by SillyLang
+data SillyExpr
+  = -- | A number
+    LitInt Int
+  | -- | A String
+    LitString T.Text
+  | -- | A Boolean
+    LitBool Bool
+  | -- | A variable
+    Variable Identifier
+  | -- | A binary operation
+    BinaryOp BinOp SillyExpr SillyExpr
+  deriving (Show, Eq)
 
-data BinOp = PlusOp
-           | MinusOp
-           | MultiplyOp
-           | EqualsOp
-           | NotEqualsOp
-           | LessThanOp
-           | GreaterThanOp
-           deriving (Show, Eq)
+-- | A binary operation
+data BinOp
+  = -- | Addition
+    PlusOp
+  | -- | Subtraction
+    MinusOp
+  | -- | Multiplication
+    MultiplyOp
+  | -- | Equality
+    EqualsOp
+  | -- | Non-Equality
+    NotEqualsOp
+  | -- | Less-Than
+    LessThanOp
+  | -- | Greater-Than
+    GreaterThanOp
+  deriving (Show, Eq)
+
+-- | A SillyLang Statement
+data Stmt
+  = -- | An expression
+    ExprStmt SillyExpr
+  | -- | Variable declaration
+    VarStmt Identifier SillyExpr
+
+type Program = [Stmt]
